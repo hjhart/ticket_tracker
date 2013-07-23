2ec79af155db57f5602ef4b71c976a12f23cb538
James Hart <hjhart@gmail.com>
2013-07-23 00:44:50 -0700

    add font as a precomipled asset.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ TicketTracker::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( general_enclosed_foundicons.woff social_foundicons.woff general_foundicons.woff)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
